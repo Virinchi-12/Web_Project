367868d241ef71102506b49a3fd4bff9df858e7c
Virinchi-12 <saivirinchi99@gmail.com>
2020-08-23 21:32:43 +0530

    first commit

---
+++ Social_Site_Project/simplesocial/groups/views.py
@@ -0,0 +1,84 @@
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+# pip install django-braces
+from braces.views import SelectRelatedMixin
+from django.urls import reverse
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from groups.models import Group,GroupMember
+from . import models
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ("name", "description")
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group,slug=self.kwargs.get("slug"))
+
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+
+        except IntegrityError:
+            messages.warning(self.request,("Warning, already a member of {}".format(group.name)))
+
+        else:
+            messages.success(self.request,"You are now a member of the {} group.".format(group.name))
+
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get("slug")
+            ).get()
+
+        except models.GroupMember.DoesNotExist:
+            messages.warning(
+                self.request,
+                "You can't leave this group because you aren't in it."
+            )
+
+        else:
+            membership.delete()
+            messages.success(
+                self.request,
+                "You have successfully left this group."
+            )
+        return super().get(request, *args, **kwargs)
+
+# class DeleteGroup(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+#     model = models.Group
+#     select_related = ("user", "groups")
+#     success_url = reverse_lazy("groups:single")
+#
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         return queryset.filter(user_id=self.request.user.id)
+#
+#     def delete(self, *args, **kwargs):
+#         messages.success(self.request, "Group Deleted")
+#         return super().delete(*args, **kwargs)
